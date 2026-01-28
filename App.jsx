7342c413e2bb6a9ffa35c8ef6af7a75eab786e63
Jasu2Funny <jasperjuhani22@gmail.com>
2026-01-28 13:47:12 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,23 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Opiskelijatiedot from '../Opiskelijatiedot'
 
 
-function Tervehdys(props) {
-  return <h1>Hei, {props.nimi}!</h1>;
-}
+const Infolista = ({ taulukko }) => {
+  return (
+    <ul>
+      {taulukko.map((asia, index) => (
+        <li key={index}>{asia}</li>
+      ))}
+    </ul>
+  );
+};
 
-function App() {
-  const opiskelija = { nimi: "Matti Meikäläinen", ika: 16, kurssi: "Reactin perusteet"}
+const App = () => {
+  const tiedot = ["React", "JavaScript", "CSS"];
+
   return (
     <div>
-      <Tervehdys nimi="Jasper" />
-      <Opiskelijatiedot opiskelija={opiskelija} />
+      <h1>Infolista</h1>
+      <Infolista taulukko={tiedot} />
     </div>
   );
-}
+};
 
-
-
-export default App
+export default App;
